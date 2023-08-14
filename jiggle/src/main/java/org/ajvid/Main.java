4ce313392be2905f33bfe869b4ecd96bbebfed87
Debowski <Adrian.Debowski@dnv.com>
2023-08-14 12:07:56 +0200

    Created gui which has button and close on pressing x button.

--- jiggle/src/main/java/org/ajvid/Main.java
+++ jiggle/src/main/java/org/ajvid/Main.java
@@ -4,9 +4,13 @@ import org.ajvid.service.JigglerService;
 
 import java.awt.*;
 
+import static org.ajvid.gui.JigglerGui.initializeGui;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException, AWTException {
-        JigglerService jigglerService = new JigglerService();
-        jigglerService.moveMouse();
+
+        initializeGui();
+        //JigglerService jigglerService = new JigglerService();
+        //jigglerService.moveMouse();
     }
 }
--- jiggle/src/main/java/org/ajvid/gui/JigglerGui.java
+++ jiggle/src/main/java/org/ajvid/gui/JigglerGui.java
@@ -1,2 +1,23 @@
-package org.ajvid.gui;public class JigglerGui {
+package org.ajvid.gui;
+
+import javax.swing.*;
+
+public class JigglerGui{
+
+    public static void initializeGui(){
+        JFrame f=new JFrame();
+
+        JButton b=new JButton("Jiggle");
+        b.setBounds(100,50,100, 40);
+
+        f.add(b);
+
+        f.setSize(300,200);
+        f.setLayout(null);
+        f.setVisible(true);
+
+        f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
+
 }
