c171c8ab73127dcc1640d18c7e50fc2ab486725c
Debowski <Adrian.Debowski@dnv.com>
2023-08-14 12:37:12 +0200

    Hardcoded to jiggle every 5 seconds.

--- jiggle/src/main/java/org/ajvid/service/JigglerService.java
+++ jiggle/src/main/java/org/ajvid/service/JigglerService.java
@@ -12,23 +12,8 @@ public class JigglerService {
     JigglerVariable jigglerVariable = new JigglerVariable();
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-    private void welcomeMessage(){
-        System.out.println("Welcome in Mouse Jiggler");
-    }
-
-    private void setJigglingFieldAndTime(){
-        jigglerVariable.setMaxXAxis(screenSize.getWidth());
-        jigglerVariable.setMaxYAxis(screenSize.getHeight());
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please provide time between mouse move: ");
-
-        jigglerVariable.setTimeDelayBetweenMove((scanner.nextInt()*1000));
-    }
 
     public void moveMouse() throws AWTException, InterruptedException {
-        welcomeMessage();
-        setJigglingFieldAndTime();
 
         Robot robot = new Robot();
         Random random = new Random();
@@ -36,7 +21,7 @@ public class JigglerService {
         while (true){
             robot.mouseMove(random.nextInt((int)jigglerVariable.getMaxXAxis()),(int)jigglerVariable.getMaxYAxis());
             try{
-                Thread.sleep(jigglerVariable.getTimeDelayBetweenMove());
+                Thread.sleep(5000);
                 System.out.println("Jiggling.");
             }catch (InterruptedException exception){
                 System.out.println("Jiggler stopped working.");
