83a576662c1bf509eb8f94a2f034ea6323612d0c
Adrian Debowski <adrian.debowski@outlook.com>
2023-08-15 14:15:49 +0200

    Added 2 buttons to JPanel.

--- jiggle/src/main/java/org/ajvid/Main.java
+++ jiggle/src/main/java/org/ajvid/Main.java
@@ -1,9 +1,11 @@
 package org.ajvid;
 
+import org.ajvid.gui.JigglerGui;
+
 public class Main {
     public static void main(String[] args){
 
-
+        JigglerGui jigglerGui = new JigglerGui();
 
 
     }
--- jiggle/src/main/java/org/ajvid/gui/JigglerGui.java
+++ jiggle/src/main/java/org/ajvid/gui/JigglerGui.java
@@ -1,4 +1,9 @@
 package org.ajvid.gui;
 
+import javax.swing.*;
+
 public class JigglerGui {
+    private JButton jiggleButton;
+    private JPanel panel1;
+    private JButton stopButton;
 }
