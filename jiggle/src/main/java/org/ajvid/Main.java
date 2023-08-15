7abac05d9962e6544f569978838bad67ea8f2e34
Adrian Debowski <adrian.debowski@outlook.com>
2023-08-15 14:11:30 +0200

    Generated gui files using Intellij

--- jiggle/src/main/java/org/ajvid/Main.java
+++ jiggle/src/main/java/org/ajvid/Main.java
@@ -1,12 +1,9 @@
 package org.ajvid;
 
-import org.ajvid.gui.JigglerGui;
-
 public class Main {
     public static void main(String[] args){
 
-        JigglerGui jigglerGui = new JigglerGui();
-        jigglerGui.initializeGui();
+
 
 
     }
