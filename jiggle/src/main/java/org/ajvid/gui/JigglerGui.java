33f5a439c59fd754331b801ba97b96f260de23d2
Adrian Debowski <adrian.debowski@outlook.com>
2023-08-15 17:30:57 +0200

    Added status label to let user know if Jiggler is working or not.

--- jiggle/src/main/java/org/ajvid/gui/JigglerGui.java
+++ jiggle/src/main/java/org/ajvid/gui/JigglerGui.java
@@ -3,6 +3,7 @@ package org.ajvid.gui;
 import org.ajvid.service.JigglerService;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,10 +19,15 @@ public class JigglerGui {
 
     public JigglerGui(){
 
+        label1.setForeground(Color.BLACK);
+        label1.setText("Waiting");
+
         final Thread thread = new Thread(new JigglerService());
         jiggleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                label1.setForeground(Color.GREEN);
+                label1.setText("Jiggling");
                 thread.start();
             }
         });
@@ -29,8 +35,11 @@ public class JigglerGui {
         stopButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                label1.setForeground(Color.RED);
+                label1.setText("Stoped");
                 thread.interrupt();
             }
         });
     }
+
 }
